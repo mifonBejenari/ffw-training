f7e64ed88a6974327395b875cb7baa3391a0b8ef
Mihail Bejenari <mihail.bejenari@ffw.com>
2023-11-16 18:28:29 +0200

    FIx page heading test

--- app/page.test.js
+++ app/page.test.js
@@ -1,5 +1,9 @@
 import { render } from '@testing-library/react';
-import { screen } from '@testing-library/dom'
-const page = require('./page');
-// const test = require("node:test");
-// const expect = require("expect");
+import Page from './page';
+
+test('CheckSum', () => {
+    const { queryByTestId } = render(
+        <Page />
+    );
+    expect(queryByTestId(/heading/i)).toBeTruthy();
+});
