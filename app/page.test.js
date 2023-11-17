e87ba6ce7d8fc606c367e6cd0be35810bd132a2f
Mihail Bejenari <mihail.bejenari@ffw.com>
2023-11-17 10:47:30 +0200

    Create posts page, and menu component

--- app/page.test.js
+++ app/page.test.js
@@ -3,6 +3,5 @@ import Page from './page';
 
 test('should render component in the page', () => {
   const page = render(<Page />);
-  // const { getByTestId } = render(<Page />);
   expect(page.queryByRole('heading')).toBeTruthy();
 });
---
+++ components/Menu/index.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Menu() {
+  return (
+    <nav>
+      <ul className={'menu'}>
+        <li>
+          <Link href={'/'}>{'Home'}</Link>
+        </li>
+        <li>
+          <Link href={'/posts'}>{'Posts'}</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
