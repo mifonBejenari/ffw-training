e1e6037f14c6b8df1981b4ae6e5974eb9ab6be39
Mihail Bejenari <mihail.bejenari@ffw.com>
2023-11-22 11:04:40 +0200

    test-random

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import styles from './page.module.css';
+import { getPosts } from '@/lib/data';
+import PostsList from '@/components/Posts/PostsList';
 
-export default function Home() {
+export const revalidate = 20;
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  if (!posts) {
+    return null;
+  }
+  // Shuffle array
+  const shuffled = posts.sort(() => 0.5 - Math.random());
+
+  // Get sub-array of first n elements after shuffled
+  let selected = shuffled.slice(0, 5);
+
   return (
     <main className={styles.main}>
       <h1 role={'heading'}>{'Home'}</h1>
+      <h2>Random posts</h2>
+      <PostsList posts={selected} />
     </main>
   );
 }
